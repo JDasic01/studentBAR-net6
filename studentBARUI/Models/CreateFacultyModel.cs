c2a094f28b6e5b21f37010a618a2ecf6daec7704
jdasic <jdasic1@gmail.com>
2022-05-01 22:40:53 +0200

    dodano za objave

---
+++ studentBARUI/Models/CreateFacultyModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace studentBARUI.Models;
+
+public class CreateFacultyModel
+{
+    [Required]
+    [MaxLength(75)]
+    public string FacultyName { get; set; }
+    [Required]
+    [MinLength(1)]
+    [Display(Name = "University")]
+    public string UniversityId { get; set; }
+    [MaxLength(500)]
+}
---
+++ studentBARUI/Models/CreatePostModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace studentBARUI.Models;
+
+public class CreatePostModel
+{
+    [Required]
+    [MaxLength(75)]
+    public string Suggestion { get; set; }
+    [Required]
+    [MinLength(1)]
+    [Display(Name = "Faculty")]
+    public string FacultyId { get; set; }
+    [MaxLength(500)]
+    public string Description { get; set; }
+}
